a283b846742fc63b00e577044a5b3d21f895e8a9
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-20 19:14:23 -0400

    Initial commit

---
+++ app/components/attribution/index.tsx
@@ -0,0 +1,22 @@
+const Attribution = () => {
+  return (
+    <div className="absolute bottom-1 text-[11px] text-gray-600 text-center mt-3">
+      Challenge by{" "}
+      <a
+        className="text-gray-700 underline"
+        href="https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS"
+        target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        className="text-gray-700 underline"
+        href="https://www.frontendmentor.io/profile/DarrickFauvel"
+        target="_blank">
+        Darrick Fauvel
+      </a>
+      .
+    </div>
+  )
+}
+export default Attribution
