86031cdb0cef1e0037907bd1a03bb7156452e353
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-21 03:34:00 -0400

    Change justification

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HiddenH1 from "./components/hidden-h1"
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center sm:justify-between p-8">
+    <main className="relative flex min-h-screen flex-col items-center sm:justify-center p-8">
       <HiddenH1>Social links profile</HiddenH1>
       <Card />
       <Attribution />
